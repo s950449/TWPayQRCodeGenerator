7828c2ed1773b43cb54a6cceb5b7fc0fb63e1922
s950449 <950449@hotmail.com.tw>
2022-04-13 22:21:51 +0800

    Add offline; Use argparse

--- app.py
+++ app.py
@@ -3,28 +3,34 @@ import requests
 import csv
 import qrcode
 import sys
+import argparse
 import mimetypes
 from PIL import Image
 from PIL import ImageDraw
 import math
 from PIL import ImageFont
 API_url = "https://i-tw.org/twpay/api"
-def call_API(name,BankID,Account):
+def call_API(BankID,Account,offline):
+    if offline:
+        ret = offline_genstr(BankID, Account)
+        return ret
     my_params = {"Bank": BankID,"Acc": Account}
     r = requests.get(API_url,params = my_params)
     if r.status_code != 200:
-        raise RuntimeError("Failed to generate code with input:",my_params)
+        raise ("Failed to generate code with input:",my_params)
     response = r.json()
     if response["Success"] != '1':
         raise RuntimeError("Failed to generate code with input:",my_params)
-    print(response)
+    #print(response)
+    return response['String']
+def gencode(data_str,name,BankID,Account):
     qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4,
 )
-    qr.add_data(response['String'])
+    qr.add_data(data_str)
     img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
     width,height = img.size
     output_width = 650
@@ -41,19 +47,25 @@ def call_API(name,BankID,Account):
     newImg.save(name+".png")
 def prepare_BICList():
     ret = dict()
-    with open("data/BIC.csv") as biclist:
+    biccsv = "data/BIC.csv"
+    file_type,file_encoding = mimetypes.guess_type(biccsv)
+    if file_type != "text/csv": 
+        raise RuntimeError("data/BIC.csv is not csv file")
+    with open(biccsv) as biclist:
         rows = csv.DictReader(biclist) 
         for row in rows:
             ret[row["BIC"]] = row["Name"]
     return ret
-def offline_gen(name,BankID,Account):
-    #TBD
-    exit()    
+def offline_genstr(BankID,Account):
+    ret = 'TWQRP://'+BankID+'NTTransfer/158/02/V1?D6='+Account+'&D5='+BankID+'&D10=901'
+    return ret   
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python app.py [csv-file]")
-        exit()
-    file_type,file_encoding = mimetypes.guess_type(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputFile",type=str,help="Input File")
+    parser.add_argument("--offline",action="store_true",help="Offline mode")
+    args = parser.parse_args()
+    offline = args.offline
+    file_type,file_encoding = mimetypes.guess_type(args.inputFile)
     if file_type != "text/csv":   
         raise RuntimeError("Input is not csv file")
     BIC_List = prepare_BICList()
@@ -61,5 +73,6 @@ if __name__ == '__main__':
     with open(sys.argv[1],newline='') as csvfile:
         rows = csv.DictReader(csvfile)
         for row in rows:
-                if row["BankID"] in BIC_List:
-                    call_API(BIC_List[row["BankID"]],row["BankID"],row["Account"])
+                if row["BankID"] in BIC_List:          
+                    data_str = call_API(row["BankID"],row["Account"],offline)
+                    gencode(data_str,BIC_List[row["BankID"]],row["BankID"],row["Account"])
